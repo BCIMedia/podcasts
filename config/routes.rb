1e76df4174a98f76206cb2b9f79d1503fb75e5c5
Michael Smith <msmith@bcimedia.com>
2018-06-05 16:13:40 -0400

    route is scoped to admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  namespace :podcast do
-    root to: "pages#home"
-    resources :series do
-      resources :episodes
+  scope :admin do
+    namespace :podcast do
+      root to: "pages#home"
+      resources :series do
+        resources :episodes
+      end
     end
   end
 end
