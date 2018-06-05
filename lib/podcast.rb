39f4e622cb53c095d1be0bdc3a4610911443a134
Michael Smith <msmith@bcimedia.com>
2018-06-05 16:13:28 -0400

    More class containers

--- lib/podcast.rb
+++ lib/podcast.rb
@@ -1,20 +1,22 @@
 require "podcast/engine"
 
-module Podcast
-  class << self
-    attr_accessor :configuration
-  end
+module Admin
+  module Podcast
+    class << self
+      attr_accessor :configuration
+    end
 
-  def self.configure
-    self.configuration ||= Configuration.new
-    yield(configuration)
-  end
-  class Configuration
-    # attr_accessor :same_setting1, :some_setting2
+    def self.configure
+      self.configuration ||= Configuration.new
+      yield(configuration)
+    end
+    class Configuration
+      # attr_accessor :same_setting1, :some_setting2
 
-    def initialize
-      # @some_setting1 = ""
-      # @some_setting2 = ""
+      def initialize
+        # @some_setting1 = ""
+        # @some_setting2 = ""
+      end
     end
   end
 end
