44f2615bd95bbfb56cf4b79da2909d492a55c5cc
Michael Smith <msmith@bcimedia.com>
2018-06-12 15:37:52 -0400

    Development files stay local

--- app/models/podcast/episode.rb
+++ app/models/podcast/episode.rb
@@ -3,7 +3,11 @@ module Podcast
     self.table_name = "podcast_episodes"
 
     has_attached_file :image
-    has_attached_file :file, path: "/#{Podcast.configuration.s3_directory}/:class/:id/:attachment/:id_:filename"
+    if Rails.env == "development"
+      has_attached_file :file
+    else
+      has_attached_file :file, path: "/#{Podcast.configuration.s3_directory}/:class/:id/:attachment/:id_:filename"
+    end
 
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
     validates_attachment_content_type :file, content_type: /\Aaudio\/.*\z/
