98dcddc71031ac1c65a55ca50eeee1abbcdf41e7
Skylar Bolton <sbolton@bcimedia.com>
2019-11-08 17:22:36 -0700

    before_filter to before_action

--- app/controllers/podcast/admin_controller.rb
+++ app/controllers/podcast/admin_controller.rb
@@ -1,6 +1,6 @@
 module Podcast
   class AdminController < ::ApplicationController
-    before_filter :authenticate
+    before_action :authenticate
 
     def authenticate
       authenticate_or_request_with_http_basic("Administration") do |username, password|
--- lib/podcast/version.rb
+++ lib/podcast/version.rb
@@ -1,3 +1,3 @@
 module Podcast
-  VERSION = '1.1.01'
+  VERSION = '1.1.02'
 end
